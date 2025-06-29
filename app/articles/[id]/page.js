d19c0a9322da0ece61e80a993ec48445f28b5892
lukas <luki.minda@gmail.com>
2025-06-29 08:53:55 +0200

    changes

--- app/articles/[id]/page.js
+++ app/articles/[id]/page.js
@@ -21,7 +21,7 @@ const page = async (props) => {
         <>
             <Header>
                 <BlogPost blog={article.data} />
-
+                <p>{blog.dataofpublishing}</p>
             </Header>
         </>
     )
